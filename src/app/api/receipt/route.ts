ea21a830c083c5bc1e1aceaee383817fdb36565d
davnpsh <git@davnpsh.dev>
2024-06-03 02:17:51 -0500

    CREATE and READ implemented

--- src/app/api/receipt/route.ts
+++ src/app/api/receipt/route.ts
@@ -1,5 +1,36 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
-export function GET() {
-  return NextResponse.json({}, { status: 200 });
+export async function GET() {
+  try {
+    const items = await prisma.receipt.findMany();
+
+    return NextResponse.json(items, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error trying to fetch receipt", error: error },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const { name, price, amount } = await request.json();
+
+    const newItem = await prisma.receipt.create({
+      data: {
+        name: name,
+        price: price,
+        amount: amount,
+      },
+    });
+
+    return NextResponse.json(newItem, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error trying to create a new item", error: error },
+      { status: 500 }
+    );
+  }
 }
