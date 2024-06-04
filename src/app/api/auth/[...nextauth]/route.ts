50c4e839b4a34f31f09618862333ae6aa4c1fea2
davnpsh <git@davnpsh.dev>
2024-06-04 03:19:13 -0500

    Removed unnecessary export

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/lib/prisma";
 
-export const authOptions = {
+const authOptions = {
   providers: [
     CredentialsProvider({
       name: "credentials",
